a935e49447d740afc4e36edf586ad080b91e34d6
fadi <fadi mabsaleh>
2018-03-20 15:43:59 +0200

    edit css in footer

--- our_project/stories/imports/Footer.js
+++ our_project/stories/imports/Footer.js
@@ -1,40 +1,45 @@
 import React from 'react';
 
-export default () => 
+export default () =>
     <div>
-        
-    <footer id="footer">
-    <div className="about">
 
-    <h3>About Us</h3>
+        <footer id="footer">
+            <div>
+                <div className="about">
 
-    <p> We are a public service company that offers horror stories to people everywhere. We encourage them to read and share their stories and exchange expiriences. Feel free to send us your feedback below!</p>
-    </div>
-                    <div className="inner">
+                    <h3>About Us</h3>
 
-                        <h3>Contact Us</h3>
+                    <p> We are a public service company that offers horror stories to people everywhere. We encourage them to read and share their stories and exchange expiriences. Feel free to send us your feedback below!</p>
+                </div>
+                <div className="socialMedia">
+                    <a className="facebook" target="_blank" title="find us on Facebook" href="http://www.facebook.com/PLACEHOLDER"><img alt="follow me on facebook" src="//login.create.net/images/icons/user/facebook_40x40.png" border="0" id="fb" /></a>
+                    <a className="instagram" target="_blank" title="follow me on instagram" href="http://www.instagram.com/PLACEHOLDER"><img alt="follow me on instagram" src="https://c866088.ssl.cf3.rackcdn.com/assets/instagram40x40.png" border="0" id="insta" /></a>
+                    <a className="twitter" target="_blank" title="follow me on twitter" href="http://www.twitter.com/PLACEHOLDER"><img alt="follow me on twitter" src="//login.create.net/images/icons/user/twitter_40x40.png" border="0" id="twitter" /></a>
+                </div>
+            </div>
+            <div className="inner">
 
-                        <form action="#" method="post">
+                <h3>Contact Us</h3>
 
-                            <div className="field half first">
-                                    <label htmlfor="name">Full Name</label>
-                                <input name="name" id="name" type="text" placeholder="Full Name"/>
-                            </div>
-                            <div className="field half">
-                                <label htmlfor="email">Email</label>
-                                <input name="email" id="email" type="email" placeholder="Email"/>
-                            </div>
-                            <div className="field">
-                                <label htmlfor="message">Message</label>
-                                <textarea name="message" id="message" rows="6" placeholder="Enter Message Here"></textarea>
-                            </div>
-                            <ul className="actions">
-                                <li><input value="Send Message" className="button alt" type="submit"/></li>
-                            </ul>
-                            <a target="_blank" title="find us on Facebook" href="http://www.facebook.com/PLACEHOLDER"><img alt="follow me on facebook" src="//login.create.net/images/icons/user/facebook_40x40.png" border="0" id= "fb" /></a>
-                            <a target="_blank" title="follow me on instagram" href="http://www.instagram.com/PLACEHOLDER"><img alt="follow me on instagram" src="https://c866088.ssl.cf3.rackcdn.com/assets/instagram40x40.png" border="0" id="insta" /></a>
-                            <a target="_blank" title="follow me on twitter" href="http://www.twitter.com/PLACEHOLDER"><img alt="follow me on twitter" src="//login.create.net/images/icons/user/twitter_40x40.png" border="0" id="twitter" /></a>
-                        </form>
+                <form action="#" method="post">
+
+                    <div className="field half first">
+                        <label htmlFor="name">Full Name</label>
+                        <input name="name" id="name" type="text" placeholder="Full Name" />
                     </div>
-                </footer>
+                    <div className="field half">
+                        <label htmlFor="email">Email</label>
+                        <input name="email" id="email" type="email" placeholder="Email" />
+                    </div>
+                    <div className="field">
+                        <label htmlFor="message">Message</label>
+                        <textarea name="message" id="message" rows="6" placeholder="Enter Message Here"></textarea>
+                    </div>
+                    <ul className="actions">
+                        <li><input value="Send Message" className="button alt" type="submit" /></li>
+                    </ul>
+
+                </form>
+            </div>
+        </footer>
     </div>
--- our_project/stories/imports/Slider.js
+++ our_project/stories/imports/Slider.js
@@ -3,19 +3,19 @@ import React from 'react';
 export default () => 
     <div>
         <section id="three" className="wrapper align-center">
-		<div class="slider-holder">
+		<div className="slider-holder">
         <span id="slider-image-1"></span>
         <span id="slider-image-2"></span>
         <span id="slider-image-3"></span>
-        <div class="image-holder">
-            <img src="banner.jpg" class="slider-image" />
-            <img src="pic01.jpg" class="slider-image" />
-            <img src="pic02.jpg" class="slider-image" />
+        <div className="image-holder">
+            <img src="banner.jpg" className="slider-image" />
+            <img src="pic01.jpg" className="slider-image" />
+            <img src="pic02.jpg" className="slider-image" />
         </div>
-        <div class="button-holder">
-            <a href="#slider-image-1" class="slider-change"></a>
-            <a href="#slider-image-2" class="slider-change"></a>
-            <a href="#slider-image-3" class="slider-change"></a>
+        <div className="button-holder">
+            <a href="#slider-image-1" className="slider-change"></a>
+            <a href="#slider-image-2" className="slider-change"></a>
+            <a href="#slider-image-3" className="slider-change"></a>
         </div>
     </div>
 			</section>
