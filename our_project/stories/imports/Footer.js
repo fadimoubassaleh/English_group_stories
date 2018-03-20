b5f18eae5b2e4d6b511dc5641e71f09ec2fcb4eb
anthony <anthony.na75@gmail.com>
2018-03-20 10:50:43 +0200

    finished all my tasks

--- our_project/stories/imports/Footer.js
+++ our_project/stories/imports/Footer.js
@@ -4,15 +4,21 @@ export default () =>
     <div>
         
     <footer id="footer">
+    <div className="about">
+
+    <h3>About Us</h3>
+
+    <p> We are a public service company that offers horror stories to people everywhere. We encourage them to read and share their stories and exchange expiriences. Feel free to send us your feedback below!</p>
+    </div>
                     <div className="inner">
 
-                        <h3>Get in touch</h3>
+                        <h3>Contact Us</h3>
 
                         <form action="#" method="post">
 
                             <div className="field half first">
-                                    <label htmlfor="name">Name</label>
-                                <input name="name" id="name" type="text" placeholder="Name"/>
+                                    <label htmlfor="name">Full Name</label>
+                                <input name="name" id="name" type="text" placeholder="Full Name"/>
                             </div>
                             <div className="field half">
                                 <label htmlfor="email">Email</label>
@@ -20,18 +26,15 @@ export default () =>
                             </div>
                             <div className="field">
                                 <label htmlfor="message">Message</label>
-                                <textarea name="message" id="message" rows="6" placeholder="Message"></textarea>
+                                <textarea name="message" id="message" rows="6" placeholder="Enter Message Here"></textarea>
                             </div>
                             <ul className="actions">
                                 <li><input value="Send Message" className="button alt" type="submit"/></li>
                             </ul>
+                            <a target="_blank" title="find us on Facebook" href="http://www.facebook.com/PLACEHOLDER"><img alt="follow me on facebook" src="//login.create.net/images/icons/user/facebook_40x40.png" border="0" id= "fb" /></a>
+                            <a target="_blank" title="follow me on instagram" href="http://www.instagram.com/PLACEHOLDER"><img alt="follow me on instagram" src="https://c866088.ssl.cf3.rackcdn.com/assets/instagram40x40.png" border="0" id="insta" /></a>
+                            <a target="_blank" title="follow me on twitter" href="http://www.twitter.com/PLACEHOLDER"><img alt="follow me on twitter" src="//login.create.net/images/icons/user/twitter_40x40.png" border="0" id="twitter" /></a>
                         </form>
-
-                        <div className="copyright">
-                            &copy; Untitled. Design: <a href="https://templated.co">TEMPLATED</a>. Images: <a href="https://unsplash.com">Unsplash</a>.
-                        </div>
-
                     </div>
                 </footer>
     </div>
-
