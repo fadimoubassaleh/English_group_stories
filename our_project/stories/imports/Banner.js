c986470d052c5889f4bde93a00b7673bf43c82fc
mitza <mitzahaddad55@gmail.com>
2018-03-22 10:49:39 +0200

    git pull

--- our_project/stories/imports/Banner.js
+++ our_project/stories/imports/Banner.js
@@ -10,28 +10,7 @@ export default () =>
 						
 					</header>
           
-					<div className="flex ">
-
-						<div>
-							<span className="icon fa-car"></span>
-							<h3>Aliquam</h3>
-							<p>Suspendisse amet ullamco</p>
-						</div>
-
-						<div>
-							<span className="icon fa-camera"></span>
-							<h3>Elementum</h3>
-							<p>className aptent taciti ad litora</p>
-						</div>
-
-						<div>
-							<span className="icon fa-bug"></span>
-							<h3>Ultrices</h3>
-							<p>Nulla vitae mauris non felis</p>
-						</div>
-
-					</div>
-
+					
 					<footer>
 						<a href="#" className="button">Get Started</a>
 					</footer>
--- our_project/stories/imports/Home1.js
+++ our_project/stories/imports/Home1.js
@@ -7,26 +7,26 @@ export default () =>
 					<div className="flex flex-2">
 						<article>
 							<div className="image round">
-								<img src="images/pic01.jpg" alt="Pic 01" />
+								<img src="images/people reading.jpg" alt="people reading" />
 							</div>
 							<header>
 								<h3>Rainer Maria Rilke</h3>
 							</header>
 							<p>"Ah how good it is to be among people who are reading!"</p>
 							<footer>
-								<a href="#" className="button">Learn More</a>
+								<a href="#" className="button">View stories</a>
 							</footer>
 						</article>
 						<article>
 							<div className="image round">
-								<img src="images/pic02.jpg" alt="Pic 02" />
+								<img src="images/write your story.jpg" alt="write your story" />
 							</div>
 							<header>
 								<h3>Write Story Now!</h3>
 							</header>
 							<p>If you can tell stories, create characters<br />devise incidents and have sinerity and passion<br />it doesn't matter a damn how you write</p>
 							<footer>
-								<a href="#" className="button">Learn More</a>
+								<a href="#" className="button">Write your Own Story Now!</a>
 							</footer>
 						</article>
 					</div>
